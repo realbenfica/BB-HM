221133088a1c505d77c22eefdceb584b3ffc4e84
“ALEX <alexgtr84@gmail.com>
2019-01-12 12:03:52 +0100

    Hero Object, Game logic functions, Resting

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,25 @@
-// Write your JS here
+let hero = {
+    name: "",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "",
+        damage: 2,
+    },
+};
+
+function rest(hero) {
+    hero.health = 10;
+    return hero;
+};
+
+function pickUpItem(hero, object) {
+    let weapon = hero.weapon;    
+    hero.inventory.push(weapon);
+    return hero;
+};
+
+function equipWeapon() {
+
+};
